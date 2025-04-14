17dcdf61c07a06efeba0d8bc2076a1ff63ced047
Altimores <98915542+Altimores@users.noreply.github.com>
2025-04-14 17:10:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-# Import the Flask application from app.py
-from app import app
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-# This file is now used as an entry point for Gunicorn
-# All the functionality has been moved to app.py
+app = FastAPI()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
